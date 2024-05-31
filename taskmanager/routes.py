3d2dbaca7d179abe186e84fa54c4c44c662a1dd4
Chris Moynan <Chrismoynan@outlook.com>
2024-05-31 09:27:30 +0000

    added tasks

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,8 +5,7 @@ from taskmanager.models import Category, Task
 
 @app.route("/")
 def home():
-    tasks = list(Task.query.order_by(Task.id).all())
-    return render_template("tasks.html", tasks=tasks)
+    return render_template("tasks.html")
 
 
 @app.route("/categories")
@@ -23,6 +22,25 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
+
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))
+
 
 @app.route("/add_task", methods=["GET", "POST"])
 def add_task():
--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -13,8 +13,4 @@ document.addEventListener("DOMContentLoaded", function() {
     // select initialization
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
-
-    // collapsible initializataion
-    let collapsibles = document.querySelectorAll(".collapsible");
-    M.Collapsible.init(collapsibles);
 });
